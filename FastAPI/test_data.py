c9073a3a957a81afa8df897a9fa63327ae9f1558
DenDevv <denisdev76@gmail.com>
2024-04-14 22:42:22 +0300

    smtp port 465

--- FastAPI/test_data.py
+++ FastAPI/test_data.py
@@ -2,7 +2,7 @@ email_from = "startmedev1@gmail.com"
 email_to = "startmedev1@gmail.com"
 password = "twqf cboj jgru sypt"
 email_host = "smtp.gmail.com"
-email_port = 587
+email_port = 465
 text = """New email!
 
 User choice: {0}
