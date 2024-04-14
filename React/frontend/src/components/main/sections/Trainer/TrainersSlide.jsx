bb1b2ffbb203bb016ab5518f66d04923642da0a1
Maniken228 <aloshakharytonov@gmail.com>
2024-04-14 15:08:43 +0300

    hotFix

--- React/frontend/src/components/main/sections/Trainer/TrainersSlide.jsx
+++ React/frontend/src/components/main/sections/Trainer/TrainersSlide.jsx
@@ -1,5 +1,7 @@
 import React, {useRef, useState} from "react";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Images from "@/image/image";
 
 function TrainerSlide({slides}) {
