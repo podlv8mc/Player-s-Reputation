f05408bd564e2a9d06dc2aae1e7dd5aab853dca2
Maniken228 <aloshakharytonov@gmail.com>
2024-04-23 14:58:44 +0300

    tableV10

--- React/frontend/src/components/table/Users/MainUsers.jsx
+++ React/frontend/src/components/table/Users/MainUsers.jsx
@@ -1,10 +1,120 @@
-import React, {useMemo, useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {useTable, usePagination, useFilters} from 'react-table';
-import items from '../users.json'
+import Modal from '@/components/main/modal/Modal';
+import Images from '@/image/image';
+import axios from "axios";
+
 
 function MainUsers() {
-    const data = useMemo(() => items.items || [], [items]);
+    const [data, setData] = useState([]);
+    const [filteredData, setFilteredData] = useState([]);
+    const [filterInput, setFilterInput] = useState('');
+    const [selectedUser, setSelectedUser] = useState(null);
+    const [editingUserData, setEditingUserData] = useState(null);
+    const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [filterInputVisible, setFilterInputVisible] = useState(false);
 
+    const [newUserData, setNewUserData] = useState({
+        username: "",
+        email: "",
+    });
+
+    const inputLabels = {
+        username: "Имя пользователя",
+        email: "Емейл",
+    };
+
+    useEffect(() => {
+        axios.get('http://213-134-31-78.netherlands.vps.ac/api/v1/users', {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem("access_token")}`
+            }
+        }).then((data) => {
+            setData(Array.isArray(data.data.items) ? data.data.items : []);
+        }).catch((data) => {
+            alert("Авторизируйтесь!")
+            window.location.href = "/"
+        })
+    }, []);
+
+    useEffect(() => {
+        setFilteredData(
+            data.filter(item =>
+                Object.values(item).some(value =>
+                    value && value.toString().toLowerCase().includes(filterInput.toLowerCase())
+                )
+            )
+        );
+    }, [data, filterInput]);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const openViewModal = (user) => {
+        setSelectedUser(user);
+    };
+
+    const closeViewModal = () => {
+        setSelectedUser(null);
+    };
+
+    const openEditModal = (user) => {
+        setEditingUserData(user);
+        setIsEditModalOpen(true);
+    };
+
+    const closeEditModal = () => {
+        setIsEditModalOpen(false);
+    };
+
+    const handleChange = e => {
+        const {name, value} = e.target;
+        setNewUserData(prevData => ({
+            ...prevData,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const createdAt = new Date().toISOString()
+        const userDataWithTimestamp = {
+            ...newUserData,
+            createdAt: createdAt
+        }
+
+        console.log(userDataWithTimestamp);
+        axios.post("http://213-134-31-78.netherlands.vps.ac/api/v1/users", userDataWithTimestamp, {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem("access_token")}`
+            }
+        })
+            .catch((error) => {
+                console.error(error);
+            });
+
+        setIsModalOpen(false);
+
+    };
+
+    const handleEditSubmit = async (e) => {
+        e.preventDefault()
+        axios.patch(`http://213-134-31-78.netherlands.vps.ac/api/v1/users/${editingUserData.id}`, editingUserData, {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem("access_token")}`
+            }
+        })
+            .then((response) => {
+                console.log(response.data);
+                setIsModalOpen(false);
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+
+    };
 
     const columns = React.useMemo(
         () => [
@@ -20,22 +130,152 @@ function MainUsers() {
         []
     );
 
-
     const {
         getTableProps,
         getTableBodyProps,
         headerGroups,
         page,
         prepareRow,
+        gotoPage,
+        pageCount,
+        state: {pageIndex},
+        canPreviousPage,
+        canNextPage,
+        previousPage,
+        nextPage,
     } = useTable(
         {
             columns,
-            data,
+            data: filteredData,
+            initialState: {pageIndex: 0},
         },
         useFilters,
         usePagination
     );
 
+    const toggleFilterInput = () => {
+        setFilterInputVisible(!filterInputVisible);
+        setFilterInput('');
+    };
+
+
+    const ModalContent = (
+        <Modal active={isModalOpen} setActive={setIsModalOpen} className="modal-scroll">
+            <button className="modal__btn-close" onClick={() => setIsModalOpen(false)}/>
+            <div className="table__modal-title">
+                Добавить пользователя
+            </div>
+            <form className="table__modal-form-wrap" onSubmit={handleSubmit}>
+                {Object.keys(newUserData).map((key, index, array) => (
+                    <div className={`table__modal-row${index === array.length - 1 ? ' hidden' : ''}`} key={key}>
+                        <label className="table__modal-cell-title" htmlFor={key}>
+                            {inputLabels[key]}
+                        </label>
+                        <input
+                            className="table__modal-cell"
+                            id={key}
+                            name={key}
+                            value={newUserData[key]}
+                            onChange={handleChange}
+                            autoComplete="off"
+                        />
+                    </div>
+                ))}
+                <button className="btn-hover table__btn" type="submit">
+                    Добавить
+                </button>
+            </form>
+        </Modal>
+    );
+
+    const EditModalContent = editingUserData && (
+        <Modal active={isEditModalOpen} setActive={setIsEditModalOpen} className="edit-modal modal-scroll">
+            <div className="table__modal-title">
+                Редактировать пользователя
+            </div>
+            <form className="table__modal-form-wrap" onSubmit={handleEditSubmit}>
+                {/* Определяем массив с информацией о полях */}
+                {Object.entries(newUserData).map(([key, value]) => (
+                    <div className="table__modal-row" key={key}>
+                        <label className="table__modal-cell-title" htmlFor={key}>{inputLabels[key]}</label>
+                        <input
+                            className="table__modal-cell"
+                            id={key}
+                            type="text"
+                            name={key}
+                            value={editingUserData[key]}
+                            onChange={(e) => setEditingUserData({...editingUserData, [key]: e.target.value})}
+                            autoComplete="off"
+                        />
+                    </div>
+                ))}
+                <div className="table__btn-row">
+                    <button className="btn-hover table__btn" onClick={() => setIsModalOpen(false)}>
+                        Отменить
+                    </button>
+                    <button className="btn-hover table__btn" type="submit">
+                        Сохранить
+                    </button>
+                </div>
+            </form>
+        </Modal>
+    );
+
+
+    const ViewModalContent = selectedUser && (
+        <Modal active={selectedUser !== null} setActive={closeViewModal} className="modal-scroll">
+            <button className="modal__btn-close" onClick={closeViewModal}/>
+            <button className="modal__btn-new table__top-btn" onClick={() => openEditModal(selectedUser)}>
+                <img src={Images.edit} alt="edit"/>
+            </button>
+            <div className="table__modal-title">
+                Информация о пользователе
+            </div>
+            <div className="table__modal-form-wrap">
+                {columns.map(column => (
+                    <div className="table__modal-row" key={column.accessor}>
+                        <div className="table__modal-cell-title">
+                            {column.Header}
+                        </div>
+                        <div className="table__modal-cell">
+                            {selectedUser && typeof column.accessor === 'function' ? column.accessor(selectedUser) : ''}
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+        </Modal>
+    );
+
+    const PreviousPageButton = ({onClick, disabled}) => (
+        <button className={`pagination__btn ${disabled ? 'disabled' : ''}`} onClick={onClick} disabled={disabled}>
+            <img src={Images.arrow} alt="arrow"/>
+        </button>
+    );
+
+    const NextPageButton = ({onClick, disabled}) => (
+        <button className={`pagination__btn ${disabled ? 'disabled' : ''}`} onClick={onClick} disabled={disabled}>
+            <img src={Images.arrow} alt="arrow"/>
+        </button>
+    );
+
+    const PageButtons = (
+        <div className="pagination__wrap">
+            <div className="pagination__box">
+                <PreviousPageButton onClick={previousPage} disabled={!canPreviousPage}/>
+                {Array.from({length: pageCount}, (_, i) => (
+                    <button
+                        key={i}
+                        onClick={() => gotoPage(i)}
+                        className={`pagination__btn-op ${pageIndex === i ? 'active' : ''}`}
+                    >
+                        {i + 1}
+                    </button>
+                ))}
+                <NextPageButton onClick={nextPage} disabled={!canNextPage}/>
+            </div>
+        </div>
+    );
 
     return (
         <main id="main" className="main">
@@ -45,11 +285,21 @@ function MainUsers() {
 
                     </div>
                     <div className="table__top">
-
+                        <input
+                            className={filterInputVisible ? "input__search" : "input__search input__search-op"}
+                            value={filterInput}
+                            onChange={e => setFilterInput(e.target.value)}
+                            placeholder="Поиск..."
+                        />
+                        <button className="table__top-btn" onClick={toggleFilterInput}>
+                            <img src={Images.search} alt="search"/>
+                        </button>
+                        <button className="table__top-btn" onClick={openModal}>
+                            <img src={Images.add} alt="add"/>
+                        </button>
                     </div>
                 </div>
             </div>
-
             <table className="table" {...getTableProps()}>
                 <thead className="table__header-wrap">
                 {headerGroups.map(headerGroup => (
@@ -66,7 +316,7 @@ function MainUsers() {
                 {page.map(row => {
                     prepareRow(row);
                     return (
-                        <tr className="table__body" {...row.getRowProps()}>
+                        <tr className="table__body" {...row.getRowProps()} onClick={() => openViewModal(row.original)}>
                             {row.cells.map((cell, index) => (
                                 <td key={index} className="table__body-cell truncate">{cell.render('Cell')}</td>
                             ))}
@@ -75,6 +325,10 @@ function MainUsers() {
                 })}
                 </tbody>
             </table>
+            {PageButtons}
+            {EditModalContent}
+            {ModalContent}
+            {ViewModalContent}
         </main>
     );
 }
