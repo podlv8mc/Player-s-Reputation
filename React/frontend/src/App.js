db2e8e63fe2dbf72c6bef8bec50f284f79e96323
Maniken228 <aloshakharytonov@gmail.com>
2024-04-30 20:44:47 +0300

    LoaderV1

--- React/frontend/src/App.js
+++ React/frontend/src/App.js
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Routes } from "react-router-dom";
 import HomePage from "@/components/HomePage";
 import Table from "@/components/table/Table";
 import Users from "@/components/table/users/Users";
 import Funds from "@/components/table/funds/Funds";
-import Cabinet from "@/components/table/cabinet/Cabinet";
+//import Cabinet from "@/components/table/cabinet/Cabinet";
+import Loader from "@/components/loader/Loader";
 
 function App() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        console.log('Component App mounted'); // Лог при монтировании компонента
+
+        const handleLoad = () => {
+            console.log('Window loaded'); // Лог при полной загрузке страницы
+            setLoading(false); // Помечаем, что загрузка завершена
+        };
+
+        window.addEventListener('load', handleLoad); // Добавляем слушатель события загрузки
+
+        return () => {
+            console.log('Component App unmounted'); // Лог при размонтировании компонента
+            window.removeEventListener('load', handleLoad); // Очищаем слушатель при размонтировании компонента
+        };
+    }, []);
+
+    useEffect(() => {
+        console.log('Loading status changed:', loading ? 'loading...' : 'finished'); // Лог изменения статуса загрузки
+    }, [loading]); // Лог при каждом изменении статуса загрузки
+
     return (
-        <Routes>
-            <Route exact path="/" element={<HomePage />} />
-            <Route path="/table" element={<Table />} />
-            <Route path="/users" element={<Users />} />
-            <Route path="/funds" element={<Funds />} />
-            {/*<Route path="/cabinet" element={<cabinet />} />*/}
-        </Routes>
+        <>
+            {loading ? <Loader /> : null} {/* Показываем Loader, если загрузка не завершена */}
+            <Routes>
+                <Route exact path="/" element={<HomePage />} />
+                <Route path="/table" element={<Table />} />
+                <Route path="/users" element={<Users />} />
+                <Route path="/funds" element={<Funds />} />
+                {/*<Route path="/cabinet" element={<cabinet />} />*/}
+            </Routes>
+        </>
     );
 }
 
---
+++ React/frontend/src/components/loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../style/loader.scss';
+
+function Loader() {
+
+    return(
+        <div className="preloader__wrap">
+        </div>
+    );
+}
+
+export default Loader;
