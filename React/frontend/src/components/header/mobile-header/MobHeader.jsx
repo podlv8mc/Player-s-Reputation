96a35ccdb23d18e155e0daefde93c5a83a5c2771
Maniken228 <aloshakharytonov@gmail.com>
2024-05-03 19:55:09 +0300

    fixV4.3

--- React/frontend/src/components/header/mobile-header/MobHeader.jsx
+++ React/frontend/src/components/header/mobile-header/MobHeader.jsx
@@ -3,7 +3,6 @@ import MobHeaderList from "@/components/header/mobile-header/MobHeaderList";
 import Images from "@/image/image";
 import {useLocation} from "react-router-dom";
 import axios from "axios";
-import List from "@/components/header/List";
 
 const MobHeader = ({handleModalOpen}) => {
     const [isOpen, setIsOpen] = useState(false);
@@ -30,13 +29,13 @@ const MobHeader = ({handleModalOpen}) => {
         if (location.pathname === "/") {
             return (
                 <>
-                    <List linkTo="home" className="globalnav" name={Images.home} alt="home" spanClass="globalnav__text"
+                    <MobHeaderList linkTo="home" className="globalnav" name={Images.home} alt="home" spanClass="globalnav__text"
                           text="Главная" isOpen={isOpen}/>
-                    <List linkTo="about" className="globalnav" name={Images.about} alt="about"
+                    <MobHeaderList linkTo="about" className="globalnav" name={Images.about} alt="about"
                           spanClass="globalnav__text" text="О нас" isOpen={isOpen}/>
-                    <List linkTo="funds" className="globalnav" name={Images.funds} alt="funds"
+                    <MobHeaderList linkTo="funds" className="globalnav" name={Images.funds} alt="funds"
                           spanClass="globalnav__text" text="Фонды" isOpen={isOpen}/>
-                    <List linkTo="trainers" className="globalnav" name={Images.trainer} alt="trainer"
+                    <MobHeaderList linkTo="trainers" className="globalnav" name={Images.trainer} alt="trainer"
                           spanClass="globalnav__text" text="Тренера" isOpen={isOpen}/>
                 </>
             );
@@ -65,12 +64,12 @@ const MobHeader = ({handleModalOpen}) => {
                 </figure>
                 <nav className="mob-header__column">
                     <ul className="mob-header__list">
-                        <MobHeaderList linkTo="/" className="globalnav globalnav-logo" name={Images.logo} alt="logo"
-                              spanClass="globalnav__text" text="Player’s Reputation" isOpen={isOpen}/>
                         {renderLists()}
                         {
                             authorization && (location.pathname === "/table" || location.pathname === "/users" || location.pathname === "/funds" || location.pathname === "/cabinet") ? (
                                 <>
+                                    <MobHeaderList linkTo="/" className="globalnav globalnav-logo" name={Images.logo} alt="logo"
+                                                   spanClass="globalnav__text" text="Player’s Reputation" isOpen={isOpen}/>
                                     <MobHeaderList linkTo="/table" className="globalnav" name={Images.tableNav} alt="table"
                                           spanClass="globalnav__text" text="Таблица" isOpen={isOpen}/>
                                     <MobHeaderList linkTo="/funds" className="globalnav" name={Images.funds__table}
