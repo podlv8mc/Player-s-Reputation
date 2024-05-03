21f845c7f787e9ae9a3f4bb935b7193aff0c1924
Maniken228 <aloshakharytonov@gmail.com>
2024-05-03 19:21:46 +0300

    fixV3.6

--- React/frontend/src/components/header/mobile-header/MobHeader.jsx
+++ React/frontend/src/components/header/mobile-header/MobHeader.jsx
@@ -31,9 +31,9 @@ const MobHeader = () => {
                     class="footer__contact-list-social footer__contact-list"
                     items={[
                         {link: "home", src: Images.home, alt: "home", text: "Главная"},
-                        {link: "#", src: Images.funds, alt: "funds", text: "Фонды"},
+                        {link: "funds", src: Images.funds, alt: "funds", text: "Фонды"},
                         {link: "contact", src: Images.trainer, alt: "trainer", text: "Тренера"},
-                        {link: "about", src: Images.about, alt: "about", text: "О нас"},
+                        {link: "trainers", src: Images.about, alt: "about", text: "О нас"},
                         {link: "#", src: Images.exit, alt: "exit", text: "Войти"},
                     ]}
                 />
