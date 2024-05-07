ada5e7b0a606dcae31a9b1eadaf2642aff696658
Maniken228 <aloshakharytonov@gmail.com>
2024-05-07 13:21:50 +0300

    tableV1

--- React/frontend/src/components/table/MainTable.jsx
+++ React/frontend/src/components/table/MainTable.jsx
@@ -409,9 +409,11 @@ function MainTable() {
                             <tr className="table__body" {...row.getRowProps()}
                                 onClick={() => openViewModal(row.original)}>
                                 {row.cells.map((cell, index) => (
-                                    <div className="table__body-cell-wrap">
-                                        <td key={index} className="table__body-cell truncate">{cell.render('Cell')}</td>
-                                    </div>
+                                    <td key={`${rowIndex}-${cellIndex}`} className="table__body-cell-wrap">
+                                        <div key={cellIndex} className="table__body-cell truncate">
+                                            {cell.render('Cell')}
+                                        </div>
+                                    </td>
                                 ))}
                             </tr>
                         );
