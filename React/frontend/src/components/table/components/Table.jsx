37f6da5dd3f3e859d8649a87a9dfa9aabd644216
Maniken228 <aloshakharytonov@gmail.com>
2024-05-10 20:00:07 +0300

    tableV4.2

--- React/frontend/src/components/table/components/Table.jsx
+++ React/frontend/src/components/table/components/Table.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-function Table() {
+function Table({apiLink, }) {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+    };
+
+    console.log(apiLink);
 
     return (
         <>
+            {windowWidth >= 800 ? (
+                <h1 className="">
+                    1
+                </h1>
+            ) : (
+                <h1 className="">
+                    2
+                </h1>
+            )}
         </>
     );
 }
--- React/frontend/src/components/table/users/UsersPage.jsx
+++ React/frontend/src/components/table/users/UsersPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Table from "@/components/table/components/Table";
 
 function UsersPage() {
     const apiLink = "users";
 
     return (
         <>
+            <Table
+                apiLink={apiLink}
+            />
         </>
     );
 }
