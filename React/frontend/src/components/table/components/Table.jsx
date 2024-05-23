8dfd86049910e330d695d73802aa7c98fe26c18d
Maniken228 <aloshakharytonov@gmail.com>
2024-05-23 13:24:41 +0300

    fixV8.2

--- React/frontend/src/components/table/components/Table.jsx
+++ React/frontend/src/components/table/components/Table.jsx
@@ -595,8 +595,11 @@ function Table({apiLink, columns, inputLabels, newUserData, setNewUserData, moda
                         <thead className="table__header-wrap">
                         {headerGroups.map(headerGroup => (
                             <tr className="table__header" {...headerGroup.getHeaderGroupProps()}>
-                                {headerGroup.headers.map(column => (
-                                    <th className="table__headers" {...column.getHeaderProps()}>
+                                {headerGroup.headers.map((column, index) => (
+                                    <th
+                                        key={index}
+                                        className={`table__headers ${index === 0 ? 'hidden' : ''}`}
+                                        {...column.getHeaderProps()}>
                                         {column.render('Header')}
                                     </th>
                                 ))}
