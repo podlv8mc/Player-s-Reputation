93a95fe6d37dc99e15f4176f1243858f2d783f0a
DenDevv <denisdev76@gmail.com>
2024-07-19 10:05:13 +0300

    few

--- FastAPI/db migration/add_funds.py
+++ FastAPI/db migration/add_funds.py
@@ -12,7 +12,6 @@ def add_funds():
     with open("./databases/userprops.nosql", "r", encoding="UTF-8") as funds_json:
         records_list = funds_json.readlines()
 
-
     with engine.sync_session() as db:
         user_props = {}
         for record in records_list[:-1]:
@@ -30,11 +29,18 @@ def add_funds():
             if user_id is not None and fund_id is not None:
                 user_props[user_id] = fund_id
 
-        for record in db.scalars(select(models.Record).options(selectinload(models.Record.created_by))).all():
-            user_id = record.created_by.old_id
-            fund = db.scalar(select(models.Fund).where(models.Fund.old_id == user_props.get(user_id)))
-            record.fund = fund
-            db.commit()
+        for record in db.scalars(
+            select(models.Record).options(selectinload(models.Record.created_by))
+        ).all():
+            user_id = record.created_by
+
+            if user_id:
+                user_id = user_id.old_id
+                fund = db.scalar(
+                    select(models.Fund).where(models.Fund.old_id == user_props.get(user_id))
+                )
+                record.fund = fund
+                db.commit()
 
 
 if __name__ == "__main__":
